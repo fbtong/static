dc440e11b285bbaa892e94e6fef04cfadae32fe6
Administrator <Administrator@5NMCABVDPUFL2SO>
2013-06-18 14:35:36 +0800

    css layout and js

--- js/fn.js
+++ js/fn.js
@@ -8,7 +8,29 @@ $("form").serialize();
  * 
  */
 
+//eg: 通过arguments获取一个函数的实际参数和预定义参数 
+function argumentsGet(a, b, c, d) {
+	var argNums = arguments.length;
+	var fnNums = argumentsGet.length;
+	
+	console.log(argNums, argNums[0], fnNums, fnNums[0]);
+}
 
 /**
  * javascript arguments.callee
  */
+function calleeObject(){
+	console.log(arguments.callee);
+}
+calleeObject();
+function create(){
+	return function(n){
+		if(n <= 1)
+			return 1;
+		return n * arguments.callee(n-1);
+	}
+}
+var result = create()(5);
+[1,2,3,4,5].map(function(n){
+    return !(n > 1)? 1 : arguments.callee(n-1)*n;
+})
