a0767043570bd16633958aafea5154f0737ef069
Administrator <Administrator@5NMCABVDPUFL2SO>
2013-08-19 17:53:21 +0800

    dialog优化！

--- js/dialog.js
+++ js/dialog.js
@@ -26,118 +26,72 @@
 		return el;
 	};
 	
-	var popDialog = function(This, dialogEl, obj) {
-		var $This = This;
-	           elHeight = $This.outerHeight(),
-	           elWidth = $This.outerWidth();
-	
-				elOffset = $This.offset();
-				
-		var dialogWidth= obj.width || settings.width;
-				
-				if(obj.overlay || settings.overlay) {
-					$('body').append(overlay);
-					screenHeight = $('.overlay').outerHeight() - 60;
-				}
-				
-				$('body').append(dialogEl);
-				
-				if(obj.content && (obj.content.constructor===Function)) {
-					obj.content.call(dialogEl, $(this));
-				}else{
-					dialogEl.innerHTML = obj.content || settings.content;
-				}
-				
-				var dialogHeight = obj.height || $(dialogEl).height();
-				
-				if(obj.screenFull || settings.screenFull) {
-					dialogElLeft = parseInt(screenWeight/2 - dialogWidth/2);
-					dialogElTop = parseInt(screenHeight/2 -  dialogHeight/2);
-					$(dialogEl).css('left', dialogElLeft+'px').css('top', dialogElTop+'px');
-				}else{
-					dialogElLeft = elOffset.left + parseInt(elWidth/2) - parseInt(dialogWidth/2);
-					dialogElTop = elOffset.top + parseInt(elHeight);
-					$(dialogEl).css('left', dialogElLeft+'px').css('top', dialogElTop+'px');
-				}
-		
-				$(dialogEl).on('click', obj.close, function(e) {
-					$(dialogEl).remove();
-					$('.overlay').remove();
-					e.preventDefault();
-					e.stopPropagation();
-				});
-				$('.'+settings.dialogClass).click(function(e) {
-					e.stopPropagation();
-				});
-	};
-	
-	$.fn.dialog = function(obj) {
-			   
-		var $body = $('body');
-		
-		var dialogEl = createEl('div');
-		
+	var popFn = function(dialogEl, obj, This) {
 		var dialogWidth= obj.width || settings.width;
 		$(dialogEl).addClass(settings.dialogClass).css('width', dialogWidth);
 		
-		if (obj.autoload) {
-			popDialog($(this), dialogEl, obj);
+		if(obj.overlay || settings.overlay) {
+			$('body').append(overlay);
+			screenHeight = $('.overlay').outerHeight() - 60;
 		}
 		
-		//$This.click(function(e) {
+		$('body').append(dialogEl);
+		
+		if(obj.content && (obj.content.constructor===Function)) {
+			obj.content.call(dialogEl, $(this));
+		}else{
+			dialogEl.innerHTML = obj.content || settings.content;
+		}
+		
+		var dialogHeight = obj.height || $(dialogEl).height();
+		
+		if (This) {
+			var $This = This,
+	        	   elHeight = $This.outerHeight(),
+	        	   elWidth = $This.outerWidth(),
+	        	   elOffset = $This.offset();
+		}
+		
+		if(obj.screenFull || settings.screenFull) {
+			dialogElTop = parseInt(screenHeight/2 -  dialogHeight/2);
+			dialogElLeft = parseInt(screenWeight/2 - dialogWidth/2);
+			$(dialogEl).css('left', dialogElLeft+'px').css('top', dialogElTop+'px');
+		}else{
+			dialogElTop = elOffset.top + parseInt(elHeight);
+			dialogElLeft = elOffset.left + parseInt(elWidth/2) - parseInt(dialogWidth/2);
+			$(dialogEl).css('left', dialogElLeft+'px').css('top', dialogElTop+'px').css('position', 'absolute');
+		}
+
+		$(dialogEl).on('click', obj.close, function(e) {
+			$(dialogEl).remove();
+			$('.overlay').remove();
+			e.preventDefault();
+			e.stopPropagation();
+		});
+		$('.'+settings.dialogClass).click(function(e) {
+			e.stopPropagation();
+		});
+	};
+	
+	$.dialog = function(obj) {
+		var dialogEl = createEl('div');
+		if (obj.autoload) {
+			popFn(dialogEl, obj);
+		}
+	};
+	
+	$.fn.dialog = function(obj) {
+		
+		var dialogEl = createEl('div');
+		
+		if (obj.autoload) {
+			popFn(dialogEl, obj, $(this));
+		}
+		
 		$('body').on('click', this.selector, function(e) {
 			e.preventDefault();
 			e.stopPropagation();
-			
-			var $This = $(this);
-			       elHeight = $This.outerHeight(),
-			       elWidth = $This.outerWidth();
-			
-			elOffset = $This.offset();
-			
-			if(obj.overlay || settings.overlay) {
-				$body.append(overlay);
-				screenHeight = $('.overlay').outerHeight() - 60;
-			}
-			
-			$body.append(dialogEl);
-			
-			if(obj.content && (obj.content.constructor===Function)) {
-				obj.content.call(dialogEl, $(this));
-			}else{
-				dialogEl.innerHTML = obj.content || settings.content;
-			}
-			
-			var dialogHeight = obj.height || $(dialogEl).height();
-			
-			if(obj.screenFull || settings.screenFull) {
-				dialogElLeft = parseInt(screenWeight/2 - dialogWidth/2);
-				dialogElTop = parseInt(screenHeight/2 -  dialogHeight/2);
-				$(dialogEl).css('left', dialogElLeft+'px').css('top', dialogElTop+'px');
-			}else{
-				dialogElLeft = elOffset.left + parseInt(elWidth/2) - parseInt(dialogWidth/2);
-				dialogElTop = elOffset.top + parseInt(elHeight);
-				$(dialogEl).css('left', dialogElLeft+'px').css('top', dialogElTop+'px').css('position', 'absolute');
-			}
-			
-			$(dialogEl).on('click', obj.close, function(e) {
-				$(dialogEl).remove();
-				$('.overlay').remove();
-				e.preventDefault();
-				e.stopPropagation();
-			});
-			/*$('body').on('click', (function(e) {
-				$(dialogEl).remove();
-				$('.overlay').remove();
-			});*/
-			$('.'+settings.dialogClass).click(function(e) {
-				e.stopPropagation();
-			});
-			
-			/*$(window).scroll(function() {
-				dialogElTop = parseInt(screenHeight/2 -  dialogHeight/2 + $(window).scrollTop());
-				$(dialogEl).css('top', dialogElTop+'px');
-			});*/
+			popFn(dialogEl, obj, $(this));
 		});
 		
 	};
